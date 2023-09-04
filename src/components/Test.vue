7fe2ebaa34ba792ecf19d7b100a3efe9b8c7df53
nodenbro <76056030+nodenbro@users.noreply.github.com>
2023-09-04 19:31:09 +0200

    testing files added

---
+++ src/components/Test.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        Testing
+    </h1>
+</template>
---
+++ src/views/TestView.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>
+        This is from the view Folder
+    </h1>
+</template>
+
+<script lang="ts">
+    console.log("Fuck you sneakily.");
+</script>
